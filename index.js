99dd6fa3aebf9a8845379c02c2bb5182e7b9493e
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-06-05 18:22:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
+  }
+  if (e.which === 39) {
+    moveDodgerRight()
   }
 })
